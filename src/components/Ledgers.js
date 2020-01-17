9d822ce9a3b7d6dd9a7fd7a225ad73df26f8fa72
Girish <girish.chennura@vaultbank.io>
2020-01-17 13:08:42 +0530

    Remove unused imports Grid

--- src/components/Ledgers.js
+++ src/components/Ledgers.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Grid from 'react-bootstrap/lib/Grid'
 import Panel from 'react-bootstrap/lib/Panel'
 import Row from 'react-bootstrap/lib/Row'
 import {injectIntl} from 'react-intl'
