367f9892649e5a58c1f8e78fae9e6f18f2539078
Girish <girish.chennura@vaultbank.io>
2020-01-28 21:23:13 +0530

    Update th ui of Error and No Match table

--- src/components/shared/Error.js
+++ src/components/shared/Error.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Grid from 'react-bootstrap/lib/Grid'
-import Row from 'react-bootstrap/lib/Row'
+import Panel from 'react-bootstrap/lib/Panel'
 import {FormattedMessage} from 'react-intl'
 
 const knownErrors = ['network']
@@ -9,16 +9,16 @@ class Error extends React.Component {
   render() {
     const id = this.props.match.params.id
     return (
-      <Grid>
-        <Row>
-          <h3>
+      <Grid fluid>
+        <Panel>
+          <h3 className="m-5 text-center text-muted">
             {id && knownErrors.indexOf(id) !== -1 ? (
               <FormattedMessage id={`error.${id}`} />
             ) : (
               <FormattedMessage id="error.unknown" />
             )}
           </h3>
-        </Row>
+        </Panel>
       </Grid>
     )
   }
